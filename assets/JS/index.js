5ff66156441345de6448dde074056a822a19f2b8
jhimi01 <talukderjhimi@gmail.com>
2024-03-19 00:14:07 +0600

    added animation and click events

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,13 +1,31 @@
+// ----------- scroll to top -----------
+const scrollButton = document.querySelector(".scroll_top");
+const toggleScrollButton = () =>
+  scrollButton.classList.toggle("hidden", window.scrollY <= 100);
+window.addEventListener("scroll", toggleScrollButton);
+scrollButton.addEventListener("click", () =>
+  window.scrollTo({ top: 0, behavior: "smooth" })
+);
+
+// ---------- pricing monthly and yearly section -----------
+function togglePricing(option) {
+  if (option === 1) {
+    document.getElementById("pricing1").classList.remove("hidden");
+    document.getElementById("pricing2").classList.add("hidden");
+  } else {
+    document.getElementById("pricing1").classList.add("hidden");
+    document.getElementById("pricing2").classList.remove("hidden");
+  }
+}
+
+// ----------- faq accordian section -------------
 document.addEventListener("DOMContentLoaded", function () {
   const accBtns = document.querySelectorAll(".acc-btn");
 
   accBtns.forEach((btn) => {
     btn.addEventListener("click", function () {
-      // Toggle active class on the clicked button
       this.classList.toggle("active");
-      // Toggle active class on the clicked button's content
       this.nextElementSibling.classList.toggle("active");
-      // Toggle visibility of plus and minus icons
       const plusIcon = this.querySelector(".icon-plus");
       const minusIcon = this.querySelector(".icon-minus");
       if (this.classList.contains("active")) {
